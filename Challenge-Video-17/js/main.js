a016642de320ab7a0b2c3fbc82545ea96bb42040
mohamod0albinnaz-coder <mohamod0albinnaz-coder>
2026-09-14 17:49:18 +0300

    Add Tasks The Name Challenge-Video

---
+++ Challenge-Video-17/js/main.js
@@ -0,0 +1,11 @@
+var the_title = "Elzreo", 
+    on_desc = "Elzero Web School" ,
+    el$datd = 25 / 10;
+
+var div4 = `
+    <h3> Hello Elzreo</h3>
+    <p>Elzero Web School</p>
+    <span>25/10</span>
+    `;
+
+    document.write( `${div4} ${div4} ${div4} ${div4}`);
