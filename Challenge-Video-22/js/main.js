45d2e82759a473d2a9976f266442f7a4cfc3e430
mohamod0albinnaz-coder <mohamod0albinnaz-coder>
2026-09-14 17:55:27 +0300

    Add Tasks The Name Challenge-Video-22

--- Challenge-Video-22/js/main.js
+++ Challenge-Video-22/js/main.js
@@ -1,1 +1,39 @@
+// challenge1
+// let a = 10;
+// let b = "20";
+// let c = 80;
+// console.log(++a + +b++ + +c++ - +a++);
+// console.log(++a + -b + +c++ - -a++ + +a);
+// console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+// 1
+// [++a] [+]
+// [++a]
+//  -Value:
+//  -Explain:
+// [+]
+//  -Explain:
+// 2
+// [++a] [+]
+// [++a]
+//  Value
+//  -Explain
+// [+]
+//  -Explain
+// 3
+// [++a] [+]
+// [++a]
+//  -Value
+//  -Explain
+// [+]
+//  -Explain
+
+
+//challeng2
+// let d = "-100";
+// let e = "20";
+// let f = 30;
+// let g = true;
+
+// console.log(-d * e );//2000
+// console.log(-d + e++ + ++f + e + g );//173
 
